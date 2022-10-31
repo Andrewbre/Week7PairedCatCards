e41579b23880022dec8c02d9e58a9f3619ce0667
Andrew Brennan <andrewbrennan216@gmail.com>
2022-10-31 12:57:43 -0400

    First two GETS cat controller

--- src/main/java/com/techelevator/controller/CatController.java
+++ src/main/java/com/techelevator/controller/CatController.java
@@ -1,8 +1,15 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.file.Path;
+import java.util.List;
 
 public class CatController {
 
@@ -14,7 +21,24 @@ public class CatController {
         this.catCardDao = catCardDao;
         this.catFactService = catFactService;
         this.catPicService = catPicService;
+
+    }
+
+    @RequestMapping(path = "/api/cards", method = RequestMethod.GET)
+    public List<CatCard> listOfCardsInUserCollect(){
+        return catCardDao.list();
     }
 
 
-}
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.GET)
+    public CatCard getCatCardById(@PathVariable long id){
+        if (catCardDao.get(id) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cat Card Not Found");
+        } else {
+            return catCardDao.get(id);
+        }
+    }
+
+    }
+
+
